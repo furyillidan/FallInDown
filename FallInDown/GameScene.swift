a631d1c7808abf0cace70dd1514b8fd4e5ec9eb7
NeoChou <killerkd@gmail.com>
2019-11-11 09:17:39 +0800

    first upload

--- FallInDown/GameScene.swift
+++ FallInDown/GameScene.swift
@@ -7,83 +7,103 @@
 //
 
 import SpriteKit
-import GameplayKit
+
+enum PlayColors {
+    static let colors = [UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1),
+                         UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1),
+                         UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1),
+                         UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1)]
+}
+
+enum SwitchState : Int {
+    case red, yellow, green, blue
+}
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var colorSwitch : SKSpriteNode!
+    var switchState = SwitchState.red
+    var currentColorIndex : Int?
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        setUpPhysics()
+        layoutScene()
+     
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func setUpPhysics () {
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2)
+        physicsWorld.contactDelegate = self
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+  
+    func layoutScene () {
+        backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1)
+        colorSwitch = SKSpriteNode(imageNamed: "ColorCircle")
+        colorSwitch.size = CGSize(width: frame.size.width/3 , height: frame.size.width/3)
+        colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
+        
+        colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: colorSwitch.size.width/2)
+        colorSwitch.physicsBody?.categoryBitMask = PhysicsCatgories.switchCategory
+        colorSwitch.physicsBody?.isDynamic = false
+        addChild(colorSwitch)
+        
+        spawnBall()
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+    func spawnBall () {
+        currentColorIndex = Int(arc4random_uniform(UInt32(4)))
+        
+        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), color: PlayColors.colors[currentColorIndex!], size: CGSize(width: 30, height: 30))
+        ball.colorBlendFactor = 1
+        ball.name = "Ball"
+        ball.size = CGSize(width: 30, height: 30)
+        ball.position = CGPoint(x: frame.midX, y: frame.maxY)
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
+        ball.physicsBody?.categoryBitMask = PhysicsCatgories.ballCategory
+        ball.physicsBody?.contactTestBitMask = PhysicsCatgories.switchCategory
+        ball.physicsBody?.collisionBitMask = PhysicsCatgories.none
+        addChild(ball)
+    }
+    
+    func turnWheel() {
+        if let newState = SwitchState(rawValue: switchState.rawValue + 1) {
+            switchState = newState
+        }else{
+            switchState = .red
         }
+        colorSwitch.run(SKAction.rotate(byAngle: .pi/2, duration: 0.25))
+    }
+    
+    func gameOver () {
+        print("GameOver!")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        turnWheel()
+    }
+}
+
+
+extension GameScene : SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        currentColorIndex = 0
+        if contactMask == PhysicsCatgories.ballCategory | PhysicsCatgories.switchCategory {
+            if let ball = contact.bodyA.node?.name == "Ball" ? contact.bodyA.node as? SKSpriteNode : contact.bodyB.node as? SKSpriteNode {
+                if currentColorIndex == switchState.rawValue {
+                    print("Correct!")
+                    ball.run(SKAction.fadeOut(withDuration: 0.25), completion:  {
+                        ball.removeFromParent()
+                        self.spawnBall()
+                    })
+                } else {
+                    gameOver()
+                }
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
---
+++ FallInDown/Setting.swift
@@ -0,0 +1,15 @@
+//
+//  Setting.swift
+//  FallInDown
+//
+//  Created by Neo Chou on 2019/11/7.
+//  Copyright © 2019 Neo Chou. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCatgories {
+    static let none : UInt32 = 0
+    static let ballCategory : UInt32 = 0x1
+    static let switchCategory : UInt32 = 0x1 << 1
+}
